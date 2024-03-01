a8fa35e7f9c7862cfa4503ee515d14563873d01b
laura.suypene@estudante.ifms.edu.br <laura.suypene@estudante.ifms.edu.br>
2024-03-01 18:31:56 -0300

    commit 01-03

--- noticias.js
+++ noticias.js
@@ -1,24 +1,17 @@
-<<<<<<< HEAD
 var http = require('http');
+var server = http.createServer(function (req, res) {
 
-var server = http.createServer(function(req,res){
-	var categoria = req.url;
-	if (categoria == '/cursos'){
-		res.end('<html><body><h1>IFMS Not&iacute;cias de Cursos</h1></body></html>');
-	} else if (categoria == '/esportes'){
-		res.end('<html><body><h1>IFMS Not&iacute;cias de Esportes</h1></body></html>');
-	}else if (categoria == '/pesquisa'){
-		res.end('<html><body><h1>IFMS Not&iacute;cias de Pesquisa</h1></body></html>');
-	}else{
-		res.end('<html><body><h1>Portal de Not&iacute;cias IFMS</h1></body></html>');
-	}
-	// res.end('<html><body><h1>Promeiro Projeto</h1></body></html>')
+    var categoria = req.url;
+    if (categoria == '/cursos') {
+        res.end("<html> <body>IFMS Not&iacutecias de Cursos</body></html>");
+    } else if (categoria == '/esportes') {
+        res.end("<html> <body> IFMS - Not&iacutecias de Esportes</body></html>");
+    } else if (categoria == '/pesquisa') {
+        res.end("<html> <body> IMFS - Not&iacutecias de Pesquisas</body></html>");
+    } else {
+        res.end("<html> <body> Portal IFMS Not&iacutecias</body></html>");
+    }
 });
 
 server.listen(3000);
-console.log('escutando a porta 3000');
-=======
-console.log("Hello Kitty")
-console.log("é")
-console.log("editado online")
->>>>>>> da7addc1bc7b7fa5304c516cf0a49f04ba1137ef
+console.log('Escutando a porta 3000');
