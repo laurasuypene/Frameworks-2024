5348ff92cc45d23f93338c3b2dc2d8f0593e14fc
laura.suypene@estudante.ifms.edu.br <laura.suypene@estudante.ifms.edu.br>
2024-06-11 17:09:48 -0300

    11/06/24

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ var consign = require("consign");
 var app = express();
 app.set('view engine','ejs'); //informa ao express que serpa usado ejs como engrenagem das views
 app.set('views','./app/views'); // indica o caminho das viwes
-app.use(express.static("/app/public"));
+app.use(express.static("/app/public/"));
 consign()
 .include('app/routes')
 .then('conf/dbConnection.js')
